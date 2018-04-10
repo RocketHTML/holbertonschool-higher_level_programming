905e2cdee1c5595539f04e52d84c4f57f51e5296
root <madisononealburke@gmail.com>
2018-04-10 20:13:46 +0000

    keep digit positive

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    digit = int(((number * number) ** (1/2)) % 10)
+    print('{}'.format(digit))
+    return digit
+
+if __name__ == '__main__':
+    print_last_digit(10)
+    print_last_digit(11)
+    print_last_digit(-10)
+    print_last_digit(-11)
