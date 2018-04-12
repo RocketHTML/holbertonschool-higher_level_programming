f8319aacf1cc141bc31a382582cde65341757d16
root <madisononealburke@gmail.com>
2018-04-12 19:46:00 +0000

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ['+', '-', '*', '/']
+    funcs = [add, sub, mul, div]
+    choice = None
+
+    for i in range(len(ops)):
+        if argv[2] == ops[i]:
+            choice = i;
+    if choice == None:
+        print('Unknown operator. Available operators: +, -, *, and /')
+        exit(1)
+
+    print('{} {} {} = {}'.format(a, ops[choice], b, funcs[choice](a, b)))
