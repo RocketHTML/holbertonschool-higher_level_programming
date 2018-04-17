6c0cdad779948df520d6e2ff2f283b26509ad7e1
root <madisononealburke@gmail.com>
2018-04-17 10:44:46 +0000

    print max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    m = None
+    for x in my_list:
+        if m == None or m < x:
+            m = x
+    return m
+
+if __name__ == '__main__':
+    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+    negatives = [-1, -2, -3, -4]
+    max_value = max_integer(my_list)
+    empty = max_integer()
+    nega = max_integer(negatives)
+    print("Max: {}".format(max_value))
+    print("Empty: {}".format(empty))
+    print("Negative Max: {}".format(nega))
