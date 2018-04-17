edd4ec7e010f024b835b1d4a8abbc76fc902a15e
root <madisononealburke@gmail.com>
2018-04-17 10:15:11 +0000

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print('')
+        return
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j + 1 == len(matrix[i]):
+                last = '\n'
+            else:
+                last = ' '
+            print('{}'.format(matrix[i][j]), end=last)
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
