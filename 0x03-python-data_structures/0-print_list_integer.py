b1f4c753281255e8f6f068657868e6d777ec3ef2
root <madisononealburke@gmail.com>
2018-04-17 20:33:14 +0000

    no string cast

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for x in my_list:
-        print('{}'.format(x))
+        print('{:d}'.format(x))
 
 if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5]
