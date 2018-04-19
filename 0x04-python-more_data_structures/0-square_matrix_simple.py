bc0fc125fb85324ee7a276ce3dd0e4a367b9a827
root <madisononealburke@gmail.com>
2018-04-19 19:40:20 +0000

    pep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,7 @@ def copy(mx, rx, i):
     rx.append(mx[i][:])
     copy(mx, rx, i + 1)
 
+
 def matrix_xy(mx, x, y):
     mx[x][y] *= mx[x][y]
     if y == len(mx[x]) - 1:
@@ -16,18 +17,20 @@ def matrix_xy(mx, x, y):
         y += 1
     return matrix_xy(mx, x, y)
 
+
 def square_matrix_simple(matrix=[]):
     clone = []
-    copy(matrix, clone, 0)
-    if type(clone) == list and type(clone[0]) == list:
+    if type(matrix) == list and len(matrix) > 0:
+        copy(matrix, clone, 0)
         return matrix_xy(clone, 0, 0)
+
 
 if __name__ == '__main__':
     matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-    ]
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+        ]
 
     new_matrix = square_matrix_simple(matrix)
     print(new_matrix)
