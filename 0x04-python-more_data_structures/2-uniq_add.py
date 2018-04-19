af16681cb1bbf946f785941be59ea7801703f149
root <madisononealburke@gmail.com>
2018-04-19 19:58:22 +0000

    simplify

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    total = 0
-    if type(my_list) != list:
-        return total
-    cln = my_list[:]
-    cln.sort()
-    for i in range(len(cln)):
-        v = i - 1
-        if cln[i] != cln[v]:
-            total += cln[i]
-    return total
+    return sum(set(my_list))
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 1, 4, 2, 5]
