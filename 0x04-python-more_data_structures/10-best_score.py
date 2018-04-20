a1fc7d7a10852b83715c00b4c4fb408e1f5b12e6
root <madisononealburke@gmail.com>
2018-04-20 02:55:26 +0000

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    vals = [tuple(reversed((a, b))) for (a, b) in a_dictionary.items()]
+    vals.sort()
+    return vals[0][1]
+
+if __name__ == '__main__':
+    best_score = __import__('10-best_score').best_score
+
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+    best_key = best_score(a_dictionary)
+    print("Best score: {}".format(best_key))
+
+    best_key = best_score(None)
+    print("Best score: {}".format(best_key))
