0c28758fa920da6fae73bb5a422bb255f778271c
root <madisononealburke@gmail.com>
2018-04-20 05:14:37 +0000

    roman numeral

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    IV = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        result += IV[roman_string[i]]
+        if i + 1 < len(roman_string) and IV[roman_string[i]] <\
+           IV[roman_string[i]]:
+            result -= IV[roman_string[i]]
+    return result
+
+
+if __name__ == '__main__':
+    roman_number = "X"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "VII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "IX"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "LXXXVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "DCCVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
