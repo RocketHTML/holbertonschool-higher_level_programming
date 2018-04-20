faaad7e2477d601bf5477e5ac42e553b94935d9a
root <madisononealburke@gmail.com>
2018-04-20 03:20:03 +0000

    multiply list with map

---
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def mutiply_list_map(my_list=[], number=0):
+    x = lambda y: y * number
+    return list(map(x, my_list))
+
+if __name__ == '__main__':
+    mutiply_list_map = __import__('11-mutiply_list_map').mutiply_list_map
+
+    my_list = [1, 2, 3, 4, 6]
+    new_list = mutiply_list_map(my_list, 4)
+    print(new_list)
+    print(my_list)
