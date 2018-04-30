e9d4c26ba155f3a09aacbbfadf67d038c945d292
root <madisononealburke@gmail.com>
2018-04-30 20:28:54 +0000

    put zero division check before type check

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,18 +4,16 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             new_list[i] = my_list_1[i] / my_list_2[i]
-        except (ValueError, TypeError):
-            print('wrong type')
-            continue
         except ZeroDivisionError:
             print('division by 0')
-            continue
+        except (ValueError, TypeError):
+            print('wrong type')
         except IndexError:
             print('out of range')
             break
         finally:
             pass
-    return new_list 
+    return new_list
 
 if __name__ == '__main__':
     my_l_1 = [10, 8, 4]
