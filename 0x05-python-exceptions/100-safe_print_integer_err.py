a84e636daa16a046852d1216ee10f8bd0fa418db
root <madisononealburke@gmail.com>
2018-04-30 20:35:45 +0000

    use double quotes for format

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@
 def safe_print_integer_err(value):
     import sys
     try:
-        print('{:d}'.format(value))
+        print("{:d}".format(value))
     except Exception as ex:
         sys.stderr.write('Exception: {}\n'.format(ex))
         return False
