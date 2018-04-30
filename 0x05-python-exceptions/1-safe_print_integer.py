8672f4319c7e802bb10ca7a1529e1cb80c8b8a3c
root <madisononealburke@gmail.com>
2018-04-30 19:26:52 +0000

    check for type error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,13 +2,13 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     else:
         return True
 
 if __name__ == '__main__':
-    value = 89
+    value = None
     has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
