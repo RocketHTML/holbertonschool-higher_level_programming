77f87749d81d5a138b73ec0d9fffc39b421a2107
root <madisononealburke@gmail.com>
2018-04-30 19:49:33 +0000

    checker test len

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -20,8 +20,6 @@ if __name__ == '__main__':
     print("nb_print: {:d}".format(nb_print))
 
     #my_list = [1, 2, 3, "Holberton", 4, 5, [1, 2, 3]]
-    #nb_print = safe_print_list_integers(my_list, len(my_list))
     #print("nb_print: {:d}".format(nb_print))
 
-    #nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
     #print("nb_print: {:d}".format(nb_print))
