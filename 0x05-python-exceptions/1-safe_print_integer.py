f0be617029d515b04b901d25b04ce6dcc2e784a4
root <madisononealburke@gmail.com>
2018-04-30 19:27:39 +0000

    check for type error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     if not has_been_print:
         print("{} is not an integer".format(value))
 
-    value = "Holberton"
+    value = []
     has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
