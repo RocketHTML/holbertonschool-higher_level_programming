373ae65bdd3cb37116b4f27a9716c33510f8f537
root <madisononealburke@gmail.com>
2018-05-06 06:11:09 +0000

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    err_int = 'size must be an integer'
+    err_neg = 'size must be >= 0'
+    if type(size) != int:
+        raise TypeError(err_int)
+    if size < 0:
+        raise ValueError(err_neg)
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
