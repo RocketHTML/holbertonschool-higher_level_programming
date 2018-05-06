8152ae43ac5bbebabf4dc09e31db7c6f3be95673
root <madisononealburke@gmail.com>
2018-05-06 06:07:08 +0000

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    err_fir = 'first_name must be a string'
+    err_las = 'last_name must be a string'
+    msg = 'My name is {} {}'
+    if type(first_name) != str:
+        raise TypeError(err_fir)
+    elif type(last_name) != str:
+        raise TypeError(err_las)
+    print(msg.format(first_name, last_name))
