8d8e662bf5710b2fbe33f1f34bb1f2a41cf898e2
root <madisononealburke@gmail.com>
2018-05-06 07:00:11 +0000

    doc

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+.
+.
+.
 
+"""
 
 def text_indentation(text):
+    """
+    .
+    .
+    .
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for i in range(len(text)):
