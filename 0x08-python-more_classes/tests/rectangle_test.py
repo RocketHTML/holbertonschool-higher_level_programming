3a2703191dc983e155f1c088d232e040a4a1b957
root <madisononealburke@gmail.com>
2018-05-07 18:28:50 +0000

    initial commit

---
+++ 0x08-python-more_classes/tests/rectangle_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class RectangleTest(unittest.TestCase):
+    """ Test case to test the Rectangle class
+
+        The complete set of tests will be:
+            optional instantiation arguments
+            width properties
+            height properties
+            number_of_instances class attr
+            print_symbol class attr
+            area instance method
+            perimeter instance method
+            __str__
+            __repr__
+            __del__
+            bigger_or_equal static method
+            square class method
+    """
