4f0b1883815f4606f295ae841ccb824d16d327f3
root <madisononealburke@gmail.com>
2018-05-09 07:00:03 +0000

    got it

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.__dict__.append('Holberton')
-    return ', '.join(magic_string.__dict__)
+    magic_string.__name__ += ':Holberton'
+    return ', '.join(magic_string.__name__.split(':')[1:])
