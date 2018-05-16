a38aed857057a2b2e4582c2a8e68ccbf17469d94
root <madisononealburke@gmail.com>
2018-05-16 06:33:06 +0000

    add type validator to base class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+	def area(self):
+		raise Exception('area() is not implemented')
+
+	def integer_validator(self, name, value):
+		type_err = '{} must be an integer'.format(name)
+		val_err = '{} must be greater than 0'.format(name)
+		if type(value) != int:
+			raise TypeError(type_err)
+		if value <= 0:
+			raise ValueError(val_err)
