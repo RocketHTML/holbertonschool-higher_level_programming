119c77653d39c0a737e0807e9abfa28361935231
root <madisononealburke@gmail.com>
2018-05-17 05:47:57 +0000

    append after line

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def append_after(filename="",
+                 search_string="",
+                 new_string=""):
+    L = []
+    with open(filename, mode='r', encoding='utf-8') as f:
+        for line in f:
+            L.append(line)
+            if search_string in line:
+                L.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as f:
+        for line in L:
+            print(line, file=f)
