9416d6785a304aef9e4c879313450e1b7ba9996b
root <madisononealburke@gmail.com>
2018-05-17 04:15:20 +0000

    checker final check isnt pep8

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+from sys import argv
+import os
+
+
 save = __import__('7-save_to_json_file').save_to_json_file
 load = __import__('8-load_from_json_file').load_from_json_file
 filename = 'add_item.json'
-from sys import argv
-import os
 
 if not os.path.exists(filename):
     save([], filename)
