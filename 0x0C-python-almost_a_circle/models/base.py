c87f537e53005beb7a37fcacfa8b1afc961ad589
root <madisononealburke@gmail.com>
2018-05-23 02:27:09 +0000

    check if none

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -105,6 +105,9 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        l = list_dictionaries
+        if l is None or len(l) == 0:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
