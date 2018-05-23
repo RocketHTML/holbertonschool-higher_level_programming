baf6d2c4a01f4b9db326f0cd35c33b8a52f2aab7
root <madisononealburke@gmail.com>
2018-05-23 00:46:35 +0000

    doc string

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""documentation placeholder"""
 from models.base import Base
 
 
 class Rectangle(Base):
+    """documentation"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
