6af32733d0901d49e2d18bbf36befe5fd7b6192e
root <madisononealburke@gmail.com>
2018-05-23 01:42:39 +0000

    doc string

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """documentation"""
     def __init__(self, size, x=0, y=0, id=None):
+        """documentation"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
