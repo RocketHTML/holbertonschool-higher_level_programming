8b685d22111dd66cbddb6fff25985ab3b8830b71
root <madisononealburke@gmail.com>
2018-06-27 00:27:26 +0000

    testing sql syntax

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- creates a test user `user_0d_1` with pw `user_0d_1_pwd` for the database
-GRANT ALL ON `*`.`*` TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
