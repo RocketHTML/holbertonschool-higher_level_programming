7169ac60d60dae171c06d715bcfbdef6bc695efd
root <madisononealburke@gmail.com>
2018-06-27 07:07:59 +0000

    use capitals

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in database that have at least one genre linked
-SELECT DISTINCT tv_shows.title as title, tv_show_genres.genre_id as genre_id
+SELECT DISTINCT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY title, genre_id;
