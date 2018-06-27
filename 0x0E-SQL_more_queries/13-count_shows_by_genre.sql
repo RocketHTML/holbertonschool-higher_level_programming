b5c43ac67ddf969f8b578a07709512075903e992
root <madisononealburke@gmail.com>
2018-06-27 07:54:59 +0000

    count amount of shows in each genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Counts the number of TV shows in each genre
+SELECT name AS genre, COUNT(*) AS number_shows
+FROM tv_genres
+LEFT OUTER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_shows DESC;
