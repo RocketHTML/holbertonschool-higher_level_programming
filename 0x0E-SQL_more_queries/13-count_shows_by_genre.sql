aabc01b05f2ce789a62a7a7e3ee2fef6cc5461dd
root <madisononealburke@gmail.com>
2018-06-27 08:02:32 +0000

    replace left having with inner join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- Counts the number of TV shows in each genre
 SELECT name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
-LEFT OUTER JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
-HAVING number_shows >= 1
 ORDER BY number_shows DESC;
