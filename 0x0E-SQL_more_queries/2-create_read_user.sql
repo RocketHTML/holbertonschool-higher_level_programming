c0c07f0b8ed221a26f0c54c2b244cecef10bda66
Madison <madisononealburke@gmail.com>
2018-06-27 01:30:29 +0000

    create user with read permissions

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates database hbtn_0d_2 and user user_0d_2
+-- user's password is user_0d_2_pwd
+-- doesn't fail if database already exists
+-- doesn't fail if user already exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
