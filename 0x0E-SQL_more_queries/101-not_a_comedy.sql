ccb870d03f27ba8338c89f4a8498953a30da54e5
root <madisononealburke@gmail.com>
2018-06-27 11:16:50 +0000

    all shows not comedy

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- find all shows not linked to comedy genre
+SELECT title FROM tv_shows
+WHERE title NOT IN
+(SELECT title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE name = "Comedy")
+ORDER BY title ASC;
