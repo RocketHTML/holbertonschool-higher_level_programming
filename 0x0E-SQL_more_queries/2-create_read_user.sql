b39a62700fb34fbeedca42a6b8ae3918d2d254cb
root <madisononealburke@gmail.com>
2018-06-27 05:10:59 +0000

    specify priviledges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,5 @@
 -- doesn't fail if user already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
