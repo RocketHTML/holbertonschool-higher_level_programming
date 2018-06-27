9643371b500694c5ed41caf369c6787fb0e6e06e
root <madisononealburke@gmail.com>
2018-06-27 07:39:15 +0000

    LEFT join combined with 'WHERE column IS NULL'

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,5 @@ SELECT title, genre_id
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL;
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, genre_id;
