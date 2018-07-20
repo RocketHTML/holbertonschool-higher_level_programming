2d3daa7b9e4ef66b2c83105f1e7acbcd6b89dcaf
root <madisononealburke@gmail.com>
2018-07-20 09:35:21 +0000

    user input in query string

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+lists all states where name matches state
+
+args:
+    mysql_username, mysql_password, database_name, state
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    if (len(argv) != 5):
+        print("(usage): <db_username> <db_password> <db_name> <state>")
+        exit()
+
+    SERVER = {'host': 'localhost', 'port': 3306,
+              'user': argv[1], 'passwd': argv[2], 'db': argv[3]}
+    db = MySQLdb.connect(**SERVER)
+    cur = db.cursor()
+    state = argv[4]
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+
+    def main():
+        res = cur.execute(query.format(state))
+        for i in cur.fetchall():
+            print(i)
+
+    main()
