a49a726fde42916abf7480630ca66abeb6d57adc
root <madisononealburke@gmail.com>
2018-07-20 08:44:07 +0000

    regex in SQL WHERE clause

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+args: mysql_username, mysql_password, database_name
+goals: Module that can do DML
+    (e.g.) so that I can add or update user info
+I'll use sql scripts for DDL
+"""
+import MySQLdb
+from sys import argv
+
+
+if (len(argv) != 4):
+    print("(usage): states.py <db_username>, <db_password>, <db_name>")
+
+SERVER = {'host': 'localhost', 'port': 3306,
+          'user': argv[1], 'passwd': argv[2], 'db': argv[3]}
+db = MySQLdb.connect(**SERVER)
+cur = db.cursor()
+
+
+def main():
+    res = cur.execute("SELECT * FROM states WHERE name REGEXP '^N+.*' ORDER BY id ASC")
+    for i in cur.fetchall():
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
