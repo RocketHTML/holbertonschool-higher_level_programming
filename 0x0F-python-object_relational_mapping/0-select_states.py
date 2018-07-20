3231b92a920a8b3876f80844412a5e9424031c32
root <madisononealburke@gmail.com>
2018-07-20 08:26:03 +0000

    fix pep8 and add passwd

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # lists all states from the database hbtn_0e_0_usa
 # args: mysql_username, mysql_password, database_name
 # goals: Module that can create, read, update, delete records from a table
@@ -7,17 +8,19 @@ import MySQLdb
 from sys import argv
 
 if (len(argv) != 4):
-	print("(usage): states.py <db_username>, <db_password>, <db_name>")
+    print("(usage): states.py <db_username>, <db_password>, <db_name>")
 
-SERVER = {'host': 'localhost', 'port': 3306, 'user': argv[1], 'passwd': argv[2], 'db': argv[3]}
+SERVER = {'host': 'localhost', 'port': 3306,
+          'user': argv[1], 'passwd': argv[2], 'db': argv[3]}
 db = MySQLdb.connect(**SERVER)
 cur = db.cursor()
 
+
 def main():
-	res = cur.execute("SELECT * FROM states ORDER BY id ASC")
-	for i in cur.fetchall():
-		print(i)
+    res = cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for i in cur.fetchall():
+        print(i)
 
 
 if __name__ == '__main__':
-	main()
+    main()
