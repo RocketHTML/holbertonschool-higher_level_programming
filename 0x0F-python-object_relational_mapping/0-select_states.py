02fe8f32786e4d18bf14a1ac57dc091b12890bc5
root <madisononealburke@gmail.com>
2018-07-20 08:21:14 +0000

    testing no passwd

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+# lists all states from the database hbtn_0e_0_usa
+# args: mysql_username, mysql_password, database_name
+# goals: Module that can create, read, update, delete records from a table
+#	(e.g.) so that I can add or update user info
+#		I'll use sql scripts to create, read, update, delete tables from a database
+import MySQLdb
+from sys import argv
+
+if (len(argv) != 4):
+	print("(usage): states.py <db_username>, <db_password>, <db_name>")
+
+SERVER = {'host': 'localhost', 'port': 3306, 'user': argv[1], 'db': argv[3]}
+db = MySQLdb.connect(**SERVER)
+cur = db.cursor()
+
+def main():
+	res = cur.execute("SELECT * FROM states ORDER BY id ASC")
+	for i in cur.fetchall():
+		print(i)
+
+
+if __name__ == '__main__':
+	main()
