84f2acca1793cdf03c8544a60e549d38417ddeb0
root <madisononealburke@gmail.com>
2018-07-21 06:53:32 +0000

    filter for name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from sys import argv
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    dbname = argv[3]
+    port = 3306
+    host = 'localhost'
+    dialect = 'mysql+mysqldb'
+    con = '{}://{}:{}@{}:{}/{}'
+    # 'mysql+pymysql://username:password@host:port/database_name'
+    formatted = con.format(dialect, user, passwd, host, port, dbname)
+    engine = create_engine(formatted)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.name == argv[4])
+    s = query.first()
+    if (s):
+        print(s.id)
+    else:
+        print("Not found")
