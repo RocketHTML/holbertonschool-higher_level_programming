8ca1723813abd07a77c39bd84dac93212e44d19a
rubyshadows <343@holbertonschool.com>
2018-08-06 10:09:52 -0700

    get header from responose

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''fetch cmdline url, and display X-Request-Id header'''
+
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        header = res.info()['X-Request-Id']
+        print(header, end='')
