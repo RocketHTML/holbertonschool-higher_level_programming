eb36dc6838dd1237e2d95e66ee90d59ccbfc109d
root <madisononealburke@gmail.com>
2018-08-06 17:14:15 +0000

    replace windows file ending

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     import urllib.request
     with urllib.request.urlopen(sys.argv[1]) as res:
         header = res.info()['X-Request-Id']
-        print(header, end='')
+        print(header)
