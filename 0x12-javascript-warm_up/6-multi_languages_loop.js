e2614707a4b5ed4987f00963708e4a55c550c20a
root <madisononealburke@gmail.com>
2018-08-29 19:31:28 +0000

    multilanguages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-let x = ['C is fun', 'Python is cool', 'Javascript is amazing']
-for (let i of x) { console.log(i) }
+let x = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i of x) { console.log(i); }
