80c1cab0decec1b7408f46e3d1c9109e67fb6453
root <madisononealburke@gmail.com>
2018-08-29 20:47:09 +0000

    try shorter method

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 let input = parseInt(process.argv[2]);
-let str = '';
+// let str = '';
 if (isNaN(input)) { console.log('Missing size'); } else {
-  str = '';
-  for (let i = 0; i < input; i++) { str += 'x'; }
-  str += '\n';
-  str = str.repeat(input);
-  str = str.substring(0, str.length - 1);
-  console.log(str);
+  // str = '';
+  for (let i = 0; i < input; i++) { /* str += 'x'; */console.log('x'.repeat(input)); }
+  // str += '\n';
+  // str = str.repeat(input);
+  // str = str.substring(0, str.length - 1);
+  // console.log(str);
 }
