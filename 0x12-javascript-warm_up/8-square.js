aafb4b09856e54814f19c2573e45f28be905fc65
root <madisononealburke@gmail.com>
2018-08-29 21:00:44 +0000

    make X captial

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ let input = parseInt(process.argv[2]);
 // let str = '';
 if (isNaN(input)) { console.log('Missing size'); } else {
   // str = '';
-  for (let i = 0; i < input; i++) { /* str += 'x'; */console.log('x'.repeat(input)); }
+  for (let i = 0; i < input; i++) { /* str += 'x'; */console.log('X'.repeat(input)); }
   // str += '\n';
   // str = str.repeat(input);
   // str = str.substring(0, str.length - 1);
