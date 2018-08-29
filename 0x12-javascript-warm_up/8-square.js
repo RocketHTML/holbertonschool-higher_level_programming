7135f9cbe079023b2d83b090db2141174ad8abb6
root <madisononealburke@gmail.com>
2018-08-29 20:40:33 +0000

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-input = parseInt(process.argv[2]);
-if (isNaN(input)) { console.log('Missing size'); }
-else {
+let input = parseInt(process.argv[2]);
+let str = '';
+if (isNaN(input)) { console.log('Missing size'); } else {
   str = '';
   for (let i = 0; i < input; i++) { str += 'x'; }
   str += '\n';
-  str = str.repeat(input)
+  str = str.repeat(input);
   str = str.substring(0, str.length - 1);
   console.log(str);
 }
