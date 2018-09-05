416b3e5a9d0b7e3c043373127f762f28e9976e18
root <madisononealburke@gmail.com>
2018-09-05 06:02:05 +0000

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-fs = require('fs')
+let fs = require('fs');
 let outfile = process.argv[4];
 let file1 = fs.readFileSync(process.argv[2], 'utf-8');
 let file2 = fs.readFileSync(process.argv[3], 'utf-8');
