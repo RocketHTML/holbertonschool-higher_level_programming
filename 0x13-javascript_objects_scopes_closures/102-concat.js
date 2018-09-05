f3b1a4165d2dda050170accb220724aad8e79400
root <madisononealburke@gmail.com>
2018-09-05 06:01:28 +0000

    concatinate files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+fs = require('fs')
+let outfile = process.argv[4];
+let file1 = fs.readFileSync(process.argv[2], 'utf-8');
+let file2 = fs.readFileSync(process.argv[3], 'utf-8');
+let output = file1 + file2;
+fs.writeFileSync(outfile, output);
