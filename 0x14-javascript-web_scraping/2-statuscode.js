b77cb26fc39bba13bd258a1e4aa0537e94f7ff78
root <madisononealburke@gmail.com>
2018-09-06 06:27:50 +0000

    make requests with node

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
